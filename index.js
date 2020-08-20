6c22408a4639d1c6e5a3da0eeba5668a48f15443
Kazeem Kadiri <kazeem.kadiri@gmail.com>
2020-08-20 12:02:54 +0100

    Setup mongoose connection to database

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+//Connect to mongodb database using mongoose
+mongoose.connect("mongodb://127.0.0.1/my_database:27017
+|| process.env.MONGO_URI,{useNewUrlParser: true,
+useUnifiedTopology: true});
 
 //Setup body-parser middleware
 app.use(bodyParser.urlencoded({extended:false});
